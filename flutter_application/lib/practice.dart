453dd858dc5d702ff4b37d4dc56b12150f5cf673
singhrishabh93 <singhrishabh1670@gmal.com>
2023-07-13 11:53:58 +0530

    Variables and Data Types

--- flutter_application/lib/practice.dart
+++ flutter_application/lib/practice.dart
@@ -2,13 +2,29 @@ import 'dart:io';
 
 void main() {
   print('welcome to dart !');
-  stdout.write('Enter Your name: ');
-  var name = stdin.readLineSync();
-  print("Welcome, $name");
+  // stdout.write('Enter Your name: ');
+  // var name = stdin.readLineSync();
+  // print("Welcome, $name");
 
-  var rishabh = new Human(); //Creating a class Object
-}
+  // var rishabh = new Human(); //Creating a class Object
 
-class Human {
+  // Declaration of Variable
+  int a = 10;
+  // a = 10;
+  print(a);
+  a = 15;
+  print(a);
+
+  String myname = 'Flutter';
+  print(myname);
+
+  num percentage = 99.98;
   
+  BigInt longValue = BigInt.parse('98765432198765432');
+
+  print(longValue);
+
+  bool isLogin = false;
 }
+
+class Human {}
