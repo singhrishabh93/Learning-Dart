8ce5dc421c882428956d9df4cb8ebc2618e74f86
singhrishabh93 <singhrishabh1670@gmal.com>
2023-07-14 10:54:22 +0530

    Var vs Dynamic (Amazing Concept)

---
+++ flutter_application/lib/practice1.dart
@@ -0,0 +1,24 @@
+void main() {
+  print('Welcome to Flutter');
+
+  String name = 'Rishabh';
+
+  //var
+
+  var subject = 'Maths';
+
+  subject =
+      'English'; //same variable doesn't tores different data types bcoz we have initialized the value at the time of declaration and further we can't change the data type.
+
+  //dyanamic
+
+  var section; // instead of var we can write dynamic also.
+
+  // dynamic section;
+
+  section = 'D'; //string
+
+  section = 17; //int data type
+
+  section = false; //boolean // same variable stores different data types bcoz we haven't initialized any value at the time of declaration.
+}
