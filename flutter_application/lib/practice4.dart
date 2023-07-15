a05be0e1868409cbce2a89bbab4bb16afcfc42db
singhrishabh93 <singhrishabh1670@gmal.com>
2023-07-15 10:37:58 +0530

    maps and hashmaps

--- flutter_application/lib/practice4.dart
+++ flutter_application/lib/practice4.dart
@@ -1,3 +1,18 @@
-void main(){
-  
+void main() {
+  print("Maps and Hashmaps");
+  // var map1 = {"Name": "Rishabh", "Address": 105}; //key must be string
+  // print(map1);
+  // print(map1["Name"]); //case sensitive
+
+  // map1["Age: "] = 21;
+  // print(map1);
+
+  var mapname = Map();
+
+  mapname["Name"] = "Rishabh";
+  mapname["Address"] = 105;
+  mapname["Age:"] = 21;
+  mapname["DOB"] = "16/05/2002";
+
+  print(mapname);
 }
