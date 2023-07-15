f6310a678244a19a856f0bf22553cf4bd298a9e9
singhrishabh93 <singhrishabh1670@gmal.com>
2023-07-15 12:09:24 +0530

    Loops in Dart

---
+++ flutter_application/lib/practice7.dart
@@ -0,0 +1,16 @@
+main() {
+  print("Loops in Dart");
+
+  // for (int i = 0; i <= 10; i++) {
+  //   print("Welcome to flutter");
+  // }
+  int a = 100;
+  do {
+    print("Dart");
+  } while (a < 50);
+
+  // while(no <= 50){
+  // print("no $no");
+  // no++;
+  // }
+}
