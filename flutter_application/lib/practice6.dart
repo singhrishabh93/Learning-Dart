d34122d697305a2e9aae95756cae376baadfad63
singhrishabh93 <singhrishabh1670@gmal.com>
2023-07-15 11:47:46 +0530

    Conditional Programming

---
+++ flutter_application/lib/practice6.dart
@@ -0,0 +1,12 @@
+main() {
+  print("Conditional Programming");
+  var a = 100;
+
+  if (a > 200) { // and (&&) or (||) 
+    print("a is greater than 200");
+  } else if (a > 50) {
+    print("a is greater than 50");
+  } else {
+    print("a is less");
+  }
+}
