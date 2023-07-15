a6167ec3de1477a8f7b883628df432a5a9d5f407
singhrishabh93 <singhrishabh1670@gmal.com>
2023-07-15 10:46:49 +0530

    Maps and Hashmaps

--- flutter_application/lib/practice4.dart
+++ flutter_application/lib/practice4.dart
@@ -11,8 +11,19 @@ void main() {
 
   mapname["Name"] = "Rishabh";
   mapname["Address"] = 105;
-  mapname["Age:"] = 21;
+  mapname["Age"] = 21;
   mapname["DOB"] = "16/05/2002";
 
+  print(mapname.isNotEmpty);
+  print(mapname.isEmpty);
+  print(mapname.length);
+  print(mapname.keys);
+  print(mapname.values);
+  print(mapname.containsKey("Name"));
+  print(mapname.containsValue("Rishabh"));
+  print(mapname.remove("DOB"));
   print(mapname);
+  
+  
+
 }
